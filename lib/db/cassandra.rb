b703b11afe4ad960423902da1367cb66b085a83f
JC <jc@gowalla.com>
2011-11-09 13:09:30 -0600

    convert db modules to classes

--- lib/db/cassandra.rb
+++ lib/db/cassandra.rb
@@ -26,18 +26,20 @@
 # caching the most recent changes to a user's cached objects. The other maps
 # cached objects back to a timestamp and is used to find delta entries to
 # remove if a cached object is marked as removed.
-module CassandraDB
+
+class TimeCache::CassandraDB
   
   MAX_DELTAS = 10_000
   
+  class << self; attr_accessor :connection end
+  
   # The Cassandra driver object to use.
-  mattr_accessor :connection
-
-  # A logger, for logging.
-  mattr_accessor :logger
-
+  def connection
+    @connection ||= TimeCache::CassandraDB.connection
+  end
+  
   # Store a cached object.
-  def self.set(data)
+  def set(data)
     data = data.to_json
     cache_key = Digest::SHA1.hexdigest(data)
 
@@ -47,7 +49,7 @@ module CassandraDB
   end
 
   # Add a cached object to a delta timeline.
-  def self.add(key, timestamp, value)
+  def add(key, timestamp, value)
     columns = { timestamp_name(timestamp) => value, cache_name(value) => timestamp.to_f.to_s }
 
     log("insert :Deltas, #{key} -> #{columns.inspect}")
@@ -55,7 +57,7 @@ module CassandraDB
   end
 
   # Remove a cached object from a delta timeline.
-  def self.rem(key, value)
+  def rem(key, value)
     log("get :Deltas, #{key}, #{cache_name(value)}")
     timestamp = connection.get(:Deltas, key, cache_name(value))
 
@@ -67,60 +69,60 @@ module CassandraDB
   end
 
   # Fetch multiple cached objects.
-  def self.get(keys)
+  def get(keys)
     log("get :Cache, #{keys.inspect}")
     connection.multi_get(:Cache, Array(keys)).values.map { |v| v['blob'] }
   end
 
   # Find all the cached objects referenced by a delta timeline.
-  def self.get_rev_range(key, most_recent, last_modified)
+  def get_rev_range(key, most_recent, last_modified)
     log("get :Deltas, #{key}, :reversed => true, :start => #{timestamp_name(most_recent)}, :finish => #{timestamp_name(last_modified)}")
     connection.get(:Deltas, key, :reversed => true, :start => timestamp_name(most_recent), :finish => timestamp_name(last_modified), :count => MAX_DELTAS).values
   end
 
   # Get a mapping of cached objects to delta timestamps for a given user.
-  def self.get_timestamp(key, value)
+  def get_timestamp(key, value)
     log("get :Deltas, #{key}, #{cache_name(value)}")
     score = connection.get(:Deltas, key, cache_name(value))
     Time.at(score.to_f).utc
   end
 
   # Remove an object from the cache.
-  def self.del(key)
+  def del(key)
     log("remove :Cache, #{key}")
     connection.remove(:Cache, key)
   end
 
   # Check if a delta cache exists for a user.
-  def self.exists(key)
+  def exists(key)
     log("exists? :Deltas, #{key}")
     connection.exists?(:Deltas, key)
   end
 
   # Given a user ID, generate a key into `:Deltas` for cached objects.
-  def self.info_key(id)
+  def info_key(id)
     ["cache", id].join(":")
   end
 
   # Given a user ID, generate a key into `:Deltas` for deleted cache objects.
-  def self.deleted_info_key(id)
+  def deleted_info_key(id)
     ["deleted", id].join(":")
   end
 
   # Given a timestamp, generate a column name for use in range queries on `:Deltas`.
-  def self.timestamp_name(timestamp)
+  def timestamp_name(timestamp)
     ["ts", timestamp.to_f].join(":")
   end
 
   # Given the SHA1 for a cached object, generate a key into `:Cache`.
-  def self.cache_name(value)
+  def cache_name(value)
     ["cache", value].join(":")
   end
 
   # Private: Log a message, if a logger is set.
-  def self.log(msg)
-    return if logger.nil?
-    logger.debug(msg)
+  def log(msg)
+    return if TimeCache.logger.nil?
+    TimeCache.logger.debug(msg)
   end
 
 end
--- lib/db/redis.rb
+++ lib/db/redis.rb
@@ -1,55 +1,61 @@
-module RedisDB
-
-  def self.info_key(id)
+class TimeCache::RedisDB
+  
+  class << self; attr_accessor :connection end
+  
+  def connection
+    @connection ||= TimeCache::RedisDB.connection
+  end
+  
+  def info_key(id)
     "bl:info_cache:info:#{id}"
   end
 
-  def self.deleted_info_key(id)
+  def deleted_info_key(id)
     "bl:info_cache:deleted_info:#{id}"
   end
 
   # set parent key that holds the data
-  def self.set(data)
+  def set(data)
     data = data.to_json
     cache_key = Digest::SHA1.hexdigest(data)
-    REDIS_FRIEND_CACHE.set(cache_key, data)
+    connection.set(cache_key, data)
     cache_key
   end
 
   # add key to a set
-  def self.add(key, timestamp, value)
-    REDIS_FRIEND_CACHE.zadd(key, timestamp.to_i, value)
+  def add(key, timestamp, value)
+    connection.zadd(key, timestamp.to_i, value)
   end
 
   # remove key from a set
-  def self.rem(key, value)
-    REDIS_FRIEND_CACHE.zrem(key, value)
+  def rem(key, value)
+    connection.zrem(key, value)
   end
 
   # get a list of keys
-  def self.get(keys)
-    REDIS_FRIEND_CACHE.mget(*keys)
+  def get(keys)
+    connection.mget(*keys)
   end
 
   # get members from a set in reverse order
-  def self.get_rev_range(key, end_pos, start_pos)
-    REDIS_FRIEND_CACHE.zrevrangebyscore(key, end_pos.to_i, start_pos.to_i)
+  def get_rev_range(key, end_pos, start_pos)
+    connection.zrevrangebyscore(key, end_pos.to_i, start_pos.to_i)
   end
 
   # get the 'score' of the member in a set
-  def self.get_timestamp(key, value)
-    score = REDIS_FRIEND_CACHE.zscore(key, value)
+  def get_timestamp(key, value)
+    score = connection.zscore(key, value)
     Time.at(score.to_i).utc
   end
 
   # delete a key
-  def self.del(key)
-    REDIS_FRIEND_CACHE.del(key)
+  def del(key)
+    connection.del(key)
   end
 
   # does a key exist
-  def self.exists(key)
-    REDIS_FRIEND_CACHE.exists(key)
+  def exists(key)
+    connection.exists(key)
   end
 
 end
